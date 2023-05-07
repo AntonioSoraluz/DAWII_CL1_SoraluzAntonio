8a0596b956ee775da25bd0194a8fe92d76084927
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-05-07 00:20:58 -0500

    Se creó el proyecto para el desarrollo de la CL1

---
+++ src/main/java/com/soraluz/DawiiCl1SoraluzAntonioApplication.java
@@ -0,0 +1,13 @@
+package com.soraluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1SoraluzAntonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1SoraluzAntonioApplication.class, args);
+	}
+
+}
